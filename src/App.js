a50b363f5607372deb2d596d5762152e77e0cd8b
RiCK BROWN <ricky.brown.00@gmail.com>
2018-10-02 14:05:42 +0100

    Project Details rough sketch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './Components/Layout/Navbar'
 import Dashboard from './Components/Dashboard/Dashboard'
+import ProjectDetails from './Components/Projects/ProjectDetails'
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route path="/" component={Dashboard} />
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/project/:id' component={ProjectDetails} />
           </Switch>
         </div>
       </BrowserRouter>
