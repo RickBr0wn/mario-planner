c4b9753a5fe1bb892634e2752700db1c29cbb411
RiCK BROWN <ricky.brown.00@gmail.com>
2018-10-02 15:00:28 +0100

    Sign In & Sign Up forms working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './Components/Layout/Navbar'
 import Dashboard from './Components/Dashboard/Dashboard'
 import ProjectDetails from './Components/Projects/ProjectDetails'
+import SignIn from './Components/Auth/SignIn'
+import SignUp from './Components/Auth/SignUp'
 
 class App extends Component {
   render() {
@@ -13,6 +15,8 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Dashboard} />
             <Route path='/project/:id' component={ProjectDetails} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
