68e0bb5652a49fe43222317cef50a024d5416ddc
RiCK BROWN <ricky.brown.00@gmail.com>
2018-10-08 19:58:52 +0100

    Firebase & redux all working

--- src/Components/Projects/ProjectList.js
+++ src/Components/Projects/ProjectList.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
+import { Link } from 'react-router-dom'
 
 const ProjectList = ({projects}) => {
   const projectMap = projects && projects.map(project => {
     return (
-      <ProjectSummary project={project} key={project.id} />
+      <Link to={'/project/' + project.id}>
+        <ProjectSummary project={project} key={project.id} />
+      </Link>
     )
   })
 
