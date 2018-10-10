fbae4e8f2b9d98fa8cc01c6a66b4245c031b0233
RiCK BROWN <ricky.brown.00@gmail.com>
2018-10-10 19:42:36 +0100

    Auth with Firebase & associated error hadling

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,9 +1,24 @@
 const initState = {
-
+  authError: null
 }
 
-const authReducer = (state = initState, action) => {
-  return state
+const authReducer = (state = initState, { type }) => {
+  console.log('login failed');
+  switch (type) {
+    case 'LOGIN_ERROR':
+      return {
+        ...state,
+        authError: 'Login Failed'
+      }
+    case 'LOGIN_SUCCESS': 
+      console.log('login ok');
+      return {
+        ...state,
+        authError: null
+      }
+    default:
+      return state
+  }
 }
 
 export default authReducer
